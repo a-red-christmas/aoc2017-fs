254ae0206c9bc44cd85da5728f9454a57b7bc2e3
Calvin <calvin@openmailbox.org>
2017-12-02 08:49:44 -0400

    break to 80 cols

--- day2/Program.fs
+++ day2/Program.fs
@@ -5,13 +5,17 @@ let part1 x =
         |> List.sum
 
 let part2 x =
-    let findDiv l = List.sumBy (fun y -> List.sumBy (fun z -> if y <> z && y % z = 0 then y / z else 0) l) l
+    // iterate over the list twice to find a divisor that isn't self and clean
+    let findDiv l = List.sumBy (fun y ->
+        List.sumBy (fun z ->
+            if y <> z && y % z = 0 then y / z else 0) l) l
     List.map findDiv x |> List.sum
 
 [<EntryPoint>]
 let main argv = 
     let lines = System.IO.File.ReadAllLines("input.txt") |> List.ofArray
-    let values = List.map (fun (x: string) -> x.Split('\t') |> List.ofArray |> List.map int) lines
+    let values = List.map (fun (x: string) ->
+        x.Split('\t') |> List.ofArray |> List.map int) lines
     let part1Res = part1 values
     let part2Res = part2 values
 
