0a0e745cea84acb6b661dc9d8c7d847639b8c9a9
Calvin <calvin@openmailbox.org>
2017-12-02 08:43:44 -0400

    part2

--- day2/Program.fs
+++ day2/Program.fs
@@ -4,12 +4,16 @@ let part1 x =
     List.map (fun y -> List.max y - List.min y) x
         |> List.sum
 
+let part2 x =
+    let findDiv l = List.sumBy (fun y -> List.sumBy (fun z -> if y <> z && y % z = 0 then y / z else 0) l) l
+    List.map findDiv x |> List.sum
+
 [<EntryPoint>]
 let main argv = 
     let lines = System.IO.File.ReadAllLines("input.txt") |> List.ofArray
     let values = List.map (fun (x: string) -> x.Split('\t') |> List.ofArray |> List.map int) lines
     let part1Res = part1 values
-    let part2Res = -1
+    let part2Res = part2 values
 
     printfn "Part 1: %d; Part 2: %d" part1Res part2Res
 
