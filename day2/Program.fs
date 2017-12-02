4eb4e5a8f0f7807ba4f66db02383ee8bc21d73b4
Calvin <calvin@openmailbox.org>
2017-12-02 07:57:45 -0400

    d1p1 solution, now to parse inputs

---
+++ day2/Program.fs
@@ -0,0 +1,15 @@
+﻿// https://adventofcode.com/2017/day/2
+
+let part1 (x: int list list) =
+    List.map (fun y -> List.max y - List.min y) x
+        |> List.sum
+
+[<EntryPoint>]
+let main argv = 
+    let values = [[5; 1; 9; 5]; [7; 5; 3]; [2; 4; 6; 8]]
+    let part1Res = part1 values
+    let part2Res = -1
+
+    printfn "Part 1: %d; Part 2: %d" part1Res part2Res
+
+    0 // return an integer exit code
