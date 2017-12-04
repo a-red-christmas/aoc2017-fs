48431ae2f81966be68ca3c4cf5426f27a7b5631d
Calvin <calvin@openmailbox.org>
2017-12-04 09:04:04 -0400

    Day 4 Part 2

--- day4/Program.fs
+++ day4/Program.fs
@@ -6,11 +6,20 @@ let part1 z =
         List.distinct x = x) |> List.filter (fun y -> y)
     List.length values
 
+let part2 z =
+    let sort (x: string) = x |> Seq.sort |> System.String.Concat
+    let values =
+        List.map (fun x -> List.map (fun y -> sort y) x) z
+        |> List.map (fun x -> List.distinct x = x)
+        |> List.filter (fun x -> x)
+    List.length values
+
 [<EntryPoint>]
 let main argv = 
     let lines = System.IO.File.ReadAllLines("input.txt") |> List.ofArray
     let split = List.map (fun (x: string) ->
         x.Split(' ') |> List.ofArray) lines
     let part1res = part1 split 
-    printfn "Part 1: %d" part1res
+    let part2res = part2 split 
+    printfn "Part 1: %d; Part 2: %d" part1res part2res
     0 // return an integer exit code
