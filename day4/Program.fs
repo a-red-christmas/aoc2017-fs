ed51d2e37675e016e3d2bda81deb0236150962b8
Calvin <calvin@openmailbox.org>
2017-12-04 07:57:43 -0400

    style points on pt1

--- day4/Program.fs
+++ day4/Program.fs
@@ -1,13 +1,16 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+let part1 z =
+    let values = z |> List.map (fun x ->
+        List.distinct x = x) |> List.filter (fun y -> y)
+    List.length values
+
 [<EntryPoint>]
 let main argv = 
     let lines = System.IO.File.ReadAllLines("input.txt") |> List.ofArray
     let split = List.map (fun (x: string) ->
         x.Split(' ') |> List.ofArray) lines
-    let values = List.map (fun x ->
-        List.distinct x = x) split |> List.filter (fun y -> y)
-    let part1 = List.length values
-    printfn "Part 1: %d" part1
+    let part1res = part1 split 
+    printfn "Part 1: %d" part1res
     0 // return an integer exit code
