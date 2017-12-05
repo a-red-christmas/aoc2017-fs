76b734e2f46208a50f88741b08e45e6ee7dbe158
Calvin <calvin@openmailbox.org>
2017-12-05 10:35:51 -0400

    Part 2
    warning: this takes a HIDEOUSLY long time to run

--- day5/Program.fs
+++ day5/Program.fs
@@ -3,18 +3,28 @@
 let rec stepThru (array: int list) position steps =
     let next = position + array.[position]
     // printfn "ap %d\tn %d\tp %d\ts %d" array.[position] next position steps
-    let nextArray = array |> List.mapi (fun x y -> if position = x then y + 1 else y)
+    let nextArray = array |> List.mapi (fun x y ->
+        if position = x then y + 1 else y)
     if (next < 0 || next >= List.length array) then
         steps + 1
     else
         stepThru nextArray next (steps + 1)
 
+let rec stepThruPart2 (array: int list) position steps =
+    let next = position + array.[position]
+    // printfn "ap %d\tn %d\tp %d\ts %d" array.[position] next position steps
+    let nextArray = array |> List.mapi (fun x y ->
+        if position = x then y + (if y > 2 then -1 else 1) else y)
+    if (next < 0 || next >= List.length array) then
+        steps + 1
+    else
+        stepThruPart2 nextArray next (steps + 1)
+
 [<EntryPoint>]
 let main argv = 
     let lines = System.IO.File.ReadAllLines("input.txt") |> List.ofArray
     let values = lines |> List.map int //[0;3;0;1;-3] //
-    let steps = stepThru values 0 0
-    let part1res = steps
-    let part2res = -1
+    let part1res = stepThru values 0 0
+    let part2res = stepThruPart2 values 0 0
     printfn "Part 1: %d; Part 2: %d" part1res part2res
-    ow.0 // return an integer exit code
+    0 // return an integer exit code
