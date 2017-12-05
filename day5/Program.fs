4ed7274d4009f9d65d7cbc549de2103fc4a40795
Calvin <calvin@openmailbox.org>
2017-12-05 13:04:29 -0400

    Use arrays for day 5 to massively improve performance
    Creating a new list each call is horrible for performance - this went down from well over 30 minutes (mostly spent on the second pass) down to ~1750ms on part 2 and ~50 on part 1.
    
    Less functional, but it's still recursive!

--- day5/Program.fs
+++ day5/Program.fs
@@ -1,30 +1,27 @@
 ﻿// https://adventofcode.com/2017/day/5
 
-let rec stepThru (array: int list) position steps =
+let rec stepThru (array: int array) position steps =
     let next = position + array.[position]
-    // printfn "ap %d\tn %d\tp %d\ts %d" array.[position] next position steps
-    let nextArray = array |> List.mapi (fun x y ->
-        if position = x then y + 1 else y)
-    if (next < 0 || next >= List.length array) then
+    array.[position] <- array.[position] + 1
+    if next < 0 || next >= Array.length array then
         steps + 1
     else
-        stepThru nextArray next (steps + 1)
+        stepThru array next (steps + 1)
 
-let rec stepThruPart2 (array: int list) position steps =
+let rec stepThruPart2 (array: int array) position steps =
     let next = position + array.[position]
-    // printfn "ap %d\tn %d\tp %d\ts %d" array.[position] next position steps
-    let nextArray = array |> List.mapi (fun x y ->
-        if position = x then y + (if y > 2 then -1 else 1) else y)
-    if (next < 0 || next >= List.length array) then
+    array.[position] <- array.[position]
+        + if array.[position] > 2 then -1 else 1
+    if next < 0 || next >= Array.length array then
         steps + 1
     else
-        stepThruPart2 nextArray next (steps + 1)
+        stepThruPart2 array next (steps + 1)
 
 [<EntryPoint>]
 let main argv = 
-    let lines = System.IO.File.ReadAllLines("input.txt") |> List.ofArray
-    let values = lines |> List.map int //[0;3;0;1;-3] //
-    let part1res = stepThru values 0 0
-    let part2res = stepThruPart2 values 0 0
+    let lines = System.IO.File.ReadAllLines("input.txt")
+    let values = lines |> Array.map int
+    let part1res = stepThru (Array.copy values) 0 0
+    let part2res = stepThruPart2 (Array.copy values) 0 0
     printfn "Part 1: %d; Part 2: %d" part1res part2res
     0 // return an integer exit code
