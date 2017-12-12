0db9a0802acb1ec8ef75ae1d18fb1252c61e388e
Calvin <calvin@openmailbox.org>
2017-12-12 15:42:16 -0400

    Day 12 Part 2

--- day12/Program.fs
+++ day12/Program.fs
@@ -1,5 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿// https://adventofcode.com/2017/day/12
 
 open System.Text.RegularExpressions
 
@@ -13,7 +12,7 @@ let str2pipe str =
     let id = int r.Groups.[1].Value
     let connects =
         r.Groups.[2].Value.Split([|", "|], System.StringSplitOptions.None)
-        |> Array.map int
+            |> Array.map int
     { Id = id; ConnectsTo = connects }
 
 // cameFrom is to prevent infinite loops
@@ -33,9 +32,33 @@ let rec part1 start finish (pipes: Pipe[]) cameFrom =
                         part1 x finish pipes (start :: cameFrom))
             Array.exists (fun x -> x) results
 
+let overlap x y = Set.intersect x y |> Set.count > 0
+
+let rec part2 (groups: List<Set<int>>) (pipes: Pipe[]) pc =
+    if Array.length pipes = pc then
+        groups
+    else
+        let current = pipes.[pc]
+        let newSet =
+            current.Id :: List.ofArray current.ConnectsTo
+                |> Set.ofList
+        let newGroups = 
+            if List.exists (fun x -> overlap x newSet) groups then
+                let m = List.filter (fun x -> overlap x newSet) groups
+                let foldedSet =
+                    Set.union
+                        newSet
+                        (List.fold (fun a e -> Set.union a e) Set.empty m)
+                foldedSet ::
+                    List.filter (fun x -> List.contains x m = false) groups
+            else
+                newSet :: groups
+        part2 newGroups pipes (pc + 1)
+
 [<EntryPoint>]
 let main argv = 
-    let lines = System.IO.File.ReadAllLines("input.txt")
+    let lines =
+        System.IO.File.ReadAllLines("input.txt")
     let input = Array.map str2pipe lines
     let part1res =
         input
@@ -44,6 +67,8 @@ let main argv =
             |> Array.filter (fun x -> fst x)
             |> Array.map (fun x-> snd x)
             |> Array.exactlyOne
-    printfn "Part 1: %d; Part 2: %d" part1res -1
+    let part2list = part2 (List.empty<Set<int>>) input 0
+    let part2res = part2list |> List.length
+    printfn "Part 1: %d; Part 2: %d" part1res part2res
     
     0 // return an integer exit code
