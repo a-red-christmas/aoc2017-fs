4e6b7be33c7ab4cfd125c33c6836abfa710a7c57
Calvin <calvin@openmailbox.org>
2017-12-15 09:55:06 -0400

    Day 15 Part 2

--- day15/Program.fs
+++ day15/Program.fs
@@ -6,22 +6,31 @@ let divisor = 2147483647UL
 
 let lower16b i = i &&& 65535UL
 
-let rec generators aPrev bPrev agreed target pc =
+let rec keepTryingFor x y multipleOf =
+    let z = (x * y) % divisor
+    if z % multipleOf = 0UL then
+        z
+    else
+        keepTryingFor z y multipleOf
+
+let rec generators aPrev bPrev aModReq bModReq agreed target pc =
     if pc = target then
         agreed
     else
-        let newA = (aPrev * genAfactor) % divisor
-        let newB = (bPrev * genBfactor) % divisor
+        let newA = keepTryingFor aPrev genAfactor aModReq
+        let newB = keepTryingFor bPrev genBfactor bModReq
         let newAgreed =
             if lower16b newA = lower16b newB then
                 agreed + 1
             else agreed
-        generators newA newB newAgreed target (pc + 1)
+        generators newA newB aModReq bModReq newAgreed target (pc + 1)
 
 [<EntryPoint>]
 let main argv = 
     let genAseed = 699UL
     let genBseed = 124UL
-    let part1res = generators genAseed genBseed 0 40000000 0
-    printfn "Part 1: %d;" part1res
+    let part1res = generators genAseed genBseed 1UL 1UL 0 40000000 0
+    let part2res = generators genAseed genBseed 4UL 8UL 0 5000000 0
+    printfn "Part 1: %d; Part 2: %d" part1res part2res
+    assert(false)
     0 // return an integer exit code
