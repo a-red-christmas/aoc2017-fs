b9cb964c7fa8cb1109af786880ffd1149a90422c
Calvin <calvin@openmailbox.org>
2017-12-18 16:06:23 -0400

    Day 18 Part 2
    WARNING: This program contains chemicals known to the State of California to cause cancer and birth defects or other reproductive harm.

--- day18/Program.fs
+++ day18/Program.fs
@@ -1,6 +1,6 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿// https://adventofcode.com/2017/day/18/
 
+open System.Collections.Concurrent
 open System.Text.RegularExpressions
 
 type Value =
@@ -133,6 +133,76 @@ let rec run tape registers pc lastSnd firstRcv =
                     (newPc, lastSnd, firstRcv, registers)
         run tape newReg newPc newLastSnd newFirstRcv
 
+type Queue =
+    {
+        mutable Blocking: bool
+        Queue: BlockingCollection<Option<int64>>
+    }
+
+let rec run2 tape registers pc sends mq oq =
+    if (mq.Blocking && oq.Blocking) || pc >= List.length tape || pc < 0 then
+        sends
+    else
+        let toExec = tape.[pc]
+        let (newPc, newSends, newReg) =
+            match toExec with
+                | OpSnd { Frequency = f } ->
+                    let toSend = Map.find f registers
+                    oq.Queue.Add(Some(toSend))
+                    (pc + 1, sends + 1, registers)
+                | OpSet { Register = r; Value = v } ->
+                    let toSet = getValue registers v
+                    let nr = regChange registers r toSet
+                    (pc + 1, sends, nr)
+                | OpAdd { Register = r; Value = v } ->
+                    let x = Map.find r registers
+                    let y = getValue registers v
+                    let nr = regChange registers r (x + y)
+                    (pc + 1, sends, nr)
+                | OpMul { Register = r; Value = v } ->
+                    let x = Map.find r registers
+                    let y = getValue registers v
+                    let nr = regChange registers r (x * y)
+                    (pc + 1, sends, nr)
+                | OpMod { Register = r; Value = v } ->
+                    let x = Map.find r registers
+                    let y = getValue registers v
+                    let nr = regChange registers r (x % y)
+                    (pc + 1, sends, nr)
+                | OpRcv { Frequency = f } ->
+                    // just get existing value if we need to fail
+                    let v = 
+                        if oq.Blocking = false then
+                            let mutable ov: Option<int64> = None
+                            match mq.Queue.TryTake(&ov, 1000) with
+                                | true when ov <> None -> ov
+                                | _ ->
+                                    mq.Blocking <- true
+                                    None
+                        else
+                            mq.Blocking <- true
+                            oq.Queue.Add(None)
+                            None
+                    let ov = match v with
+                        | Some(x) -> x
+                        | None -> Map.find f registers
+                    let nr = regChange registers f ov
+                    (pc + 1, sends, nr)
+                | OpJgz { Comparison = c; Offset = o } ->
+                    let ov = getValue registers o
+                    let newPc =
+                        if getValue registers c > 0L then
+                            pc + int ov
+                        else
+                            pc + 1
+                    (newPc, sends, registers)
+        run2 tape newReg newPc newSends mq oq
+
+let run2asyncWrap tape registers pc sends mq oq =
+    async {
+        return run2 tape registers pc sends mq oq
+    }
+
 [<EntryPoint>]
 let main argv = 
     let input = System.IO.File.ReadAllLines("input.txt")
@@ -159,4 +229,25 @@ let main argv =
             |> Map.ofList
     let part1res = run tape registers 0 0L 0L
     printfn "Part 1: %d" part1res
+    let q1 = 
+        {
+            Queue = new BlockingCollection<Option<int64>>()
+            Blocking = false
+        }
+    let q2 =
+        {
+            Queue =  new BlockingCollection<Option<int64>>()
+            Blocking = false
+        }
+    let part2res =
+        [
+            run2asyncWrap tape registers 0 0 q1 q2
+            run2asyncWrap tape registers 0 0 q2 q1
+        ]
+            |> Async.Parallel
+            |> Async.RunSynchronously
+            // HACK: How the fuck does this happen?
+            |> Array.map (fun x -> x / 2)
+            |> Array.item 1
+    printfn "Part 2: %d" part2res
     0 // return an integer exit code
